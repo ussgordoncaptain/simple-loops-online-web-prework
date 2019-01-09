bb74b60d7ca76c8bf2dd16cef8cd9a16ccd47644
amusing-bolt-9198 <edmund.nelson.b@gmail.com>
2019-01-09 00:17:24 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -11,6 +11,9 @@ def loop_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   x=0
   loop
+    if x > number_of_times
+      break
+    end
     puts phrase
     
   end
